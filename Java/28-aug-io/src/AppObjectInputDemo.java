17a4466e80dc361a15cbc643c5007572b3721ae5
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-08-28 11:56:18 +0530

    28Aug 11:56

--- Java/28-aug-io/src/AppObjectInputDemo.java
+++ Java/28-aug-io/src/AppObjectInputDemo.java
@@ -7,7 +7,6 @@ public class AppObjectInputDemo {
 	public static void main(String[] args) throws IOException, ClassNotFoundException{
 		ObjectInputStream ois=new ObjectInputStream(new FileInputStream("likit.dat"));
 		Employee emp= (Employee) ois.readObject();
-		
 		System.out.println(emp);
 	}
 
