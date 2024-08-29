d421dc37363b40f9c14d0d7b2233118b147815f1
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-08-29 11:57:35 +0530

    29Aug 11:57

--- Java/29-aug-jdbc-1/src/App2.java
+++ Java/29-aug-jdbc-1/src/App2.java
@@ -1,20 +1,22 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class App {
+public class App2 {
 
 	public static void main(String[] args) throws SQLException {
 		Scanner sc=new Scanner(System.in);
 		String username=sc.nextLine();
 		String password=sc.nextLine();
 		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","hr");
-		Statement st = con.createStatement();
-		String sql="select * from user_master where username='"+username+"' and password='"+password+"'";
-		ResultSet rs = st.executeQuery(sql);
+		PreparedStatement st = con.prepareStatement("SELECT * FROM USER_MASTER WHERE USERNAME=? AND PASSWORD=?");
+		st.setString(1, username);
+		st.setString(2, password);
+		ResultSet rs = st.executeQuery();
 		if(rs.next())
 		{
 			System.out.println("Login is successful");
