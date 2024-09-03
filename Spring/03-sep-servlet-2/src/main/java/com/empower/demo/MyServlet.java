b78c6175dfc134a6ee126de37455554e93af9d66
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-09-03 10:57:39 +0530

    03Sep 10:57

---
+++ Spring/03-sep-servlet-2/src/main/java/com/empower/demo/MyServlet.java
@@ -0,0 +1,44 @@
+package com.empower.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int no1=Integer.parseInt(request.getParameter("no1"));
+		int no2=Integer.parseInt(request.getParameter("no2"));
+		int result=no1+no2;
+		PrintWriter out = response.getWriter();
+		String output=String.format("The sum of %d and %d is %d", no1, no2, result);
+		out.print(output);
+	}
+
+}
