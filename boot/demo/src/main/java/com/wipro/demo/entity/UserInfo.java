a875beb4399871efc097f1ddc6d84db3e9ab7ad7
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-09-10 10:59:40 +0530

    10Sep 10:59

--- boot/demo/src/main/java/com/wipro/demo/entity/UserInfo.java
+++ boot/demo/src/main/java/com/wipro/demo/entity/UserInfo.java
@@ -2,8 +2,10 @@ package com.wipro.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "JAG_USER")
 public class UserInfo
 {
 	@Id
