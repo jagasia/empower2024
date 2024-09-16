ba9786109ddf691fbb8f86fcc33b2dc0e6019486
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-09-16 17:25:39 +0530

    16Sep 17:25

---
+++ reactjs/demo1/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div class="p-5 bg-primary text-white">
+      <h1>This is our title</h1>
+    </div>
+    <div className='row'>
+      <div className='col-sm-4'>
+        <img width="100" src="images/empower.jfif" />
+      </div>
+      <div className='col-sm-4'>2</div>
+      <div className='col-sm-4'>3</div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
