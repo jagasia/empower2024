b2bb815dd69a27bba73e75e576c4a51898f06f12
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-09-19 10:15:55 +0530

    19Sep 10:15

---
+++ reactjs/routing-demo/src/App.js
@@ -0,0 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Components/About';
+import { Contact } from './Components/Contact';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Logout from './Components/Logout';
+import Nav from './Components/Nav';
+import Signup from './Components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='p-5 bg-primary text-white'>
+        <h2>Routing demo</h2>
+      </div>
+      <Nav />
+      <div className='row'>
+      <div className='col-sm-4'></div>
+      <div className='col-sm-4'>
+        <Routes>
+          <Route path='/' Component={Home } />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' Component={Contact} />
+          <Route path='/login' Component={Login} />
+          <Route path='/signup' Component={Signup} />
+          <Route path='/logout' Component={Logout} />
+        </Routes>
+      </div>
+      <div className='col-sm-4'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs/routing-demo/src/Components/Nav.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Home</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="mynavbar">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/contact">Contact Us</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/signup">Sign Up</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
