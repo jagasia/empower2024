e08be4f01faf16700992c9708725fe7dd959b680
jagasia <rjagadeeswaran@yahoo.com>
2024-09-25 10:51:50 +0530

    25Sep 10:51

--- boot/image-demo/src/main/java/com/empower/demo/controller/ProductController.java
+++ boot/image-demo/src/main/java/com/empower/demo/controller/ProductController.java
@@ -2,7 +2,11 @@ package com.empower.demo.controller;
 
 import java.util.List;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.empower.demo.entity.Product;
 import com.empower.demo.service.ProductService;
+
 
 @RestController
 @RequestMapping("/product")
@@ -48,5 +53,20 @@ public class ProductController {
 		return ps.delete(id);
 	}
 	
+	//ensure that the following is added to pom.xml
+	/*
+	 * <dependency> <groupId>commons-io</groupId>
+	 * <artifactId>commons-io</artifactId> <version>2.11.0</version> </dependency>
+	 */
+	
+	
+	@GetMapping(value="/download/{id}",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public Resource getMethodName(@PathVariable("id")Integer id) {
+		Product product=ps.read(id);
+		String picture=product.getPicture();
+		byte []data=Base64.decodeBase64(picture);
+	    ByteArrayResource resource = new ByteArrayResource(data);
+		return resource;
+	}
 	
 }
